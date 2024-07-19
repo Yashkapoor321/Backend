d9e35179ef3987fa4f0ecf87343b38a087079411
Yash Kapoor <yashkapur1996@gmail.com>
2024-07-20 00:04:42 +0530

    cloud database is attached

--- job-app/index.js
+++ job-app/index.js
@@ -6,7 +6,7 @@ const port = 10000
 const app = express()
 
 
-mongoose.connect("mongodb://localhost:27017/job-app")              // job-app is a database 
+mongoose.connect("mongodb+srv://yash88887777:cVjFojPoZ5aegzKJ@cluster0.f3c4ma6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")             
 .then(() => console.log("Database connected Sucessfully"))
 .catch((err) => console.log("Error is present", err))
 
